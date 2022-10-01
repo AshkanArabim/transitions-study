a7178c577c3597353c2f1338346059996081963e
AshkanArabim <ashkan.arabim@gmail.com>
2022-10-01 00:33:02 -0600

    fix webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import("./reset.css");
+import("./style.css");
+
+const navBtns = Array.from(document.querySelectorAll(".navbtn"));
+
+for (navBtnIndex in navBtns) {
+  navBtnIndex.addEventListener("hover", () => {
+    navBtns[navBtnIndex].classList.remove("");
+  });
+}
